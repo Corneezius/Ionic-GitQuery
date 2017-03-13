e0ca4261aba9f03bdc34bc4a256080d607a1c246
Corneezius <cfr2116@columbia.edu>
2017-03-13 01:08:06 -0400

    fi final error

--- src/pages/organizations/organizations.ts
+++ src/pages/organizations/organizations.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-organizations',
-  templateUrl: 'organisations.html'
+  templateUrl: 'organizations.html'
 })
 export class OrganizationsPage {
 
