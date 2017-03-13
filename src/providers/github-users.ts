a137b18791ed94c51b1fed5d103f6cf93dfb2354
Corneezius <cfr2116@columbia.edu>
2017-03-13 01:38:34 -0400

    update provider page

--- src/providers/github-users.ts
+++ src/providers/github-users.ts
@@ -13,7 +13,12 @@ export class GithubUsers {
 
   // Load all github users
   load(): Observable<User[]> {
-    return this.http.get(`${this.githubApiUrl}/users`)
-      .map(res => <User[]>res.json());
+  
+  }
+
+  // Get github user by providing login(username)
+  loadDetails(login: string): Observable<User> {
+    return this.http.get(`${this.githubApiUrl}/users/${login}`)
+      .map(res => <User>(res.json()))
   }
 }
