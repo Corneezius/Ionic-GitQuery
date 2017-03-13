a248462ea354ce3b54ccb28427ffba5fb9979ab4
Corneezius <cfr2116@columbia.edu>
2017-03-13 00:57:30 -0400

    update component pages

--- src/pages/organizations/organizations.ts
+++ src/pages/organizations/organizations.ts
@@ -1,22 +1,15 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Organizations page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-organizations',
-  templateUrl: 'organizations.html'
+  templateUrl: 'organisations.html'
 })
-export class OrganizationsPage {
+export class OrganisationsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OrganizationsPage');
+    console.log('Hello Organisations Page');
   }
-
 }
